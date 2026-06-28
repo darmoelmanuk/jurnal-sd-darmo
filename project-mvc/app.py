d4e80861dd6b10aee1a06cb79448325944ca6b07
Darmo Wiyono <2313020200@mhs.univ.ac.id>
2026-06-28 22:50:20 +0700

    [FEAT] Implementasi environment variables pada Dockerfile

--- project-mvc/app.py
+++ project-mvc/app.py
@@ -1,4 +1,11 @@
 from controllers.user_controller import run_app
+import os
+
+user_name = os.getenv("APP_USER", "Guest")
+app_env = os.getenv("APP_ENV", "Development")
+
+print(f"Halo {user_name}! Aplikasi ini berjalan di dalam kontainer Docker.\n")
+print(f"Environment : {app_env}\n")
 
 if __name__ == "__main__":
     run_app()
